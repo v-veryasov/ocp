96920616a969f39f082699a115426cdafd9ebcfa
vladislav_veryasov <verasoww@gmail.com>
2021-03-10 17:47:31 +0300

    ---

--- src/main/java/leedCode/LFUCache.java
+++ src/main/java/leedCode/LFUCache.java
@@ -1,59 +1,80 @@
 package leedCode;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class LFUCache {
 
-    private final HashMap<Integer, Integer> map; // K, V
-    private final HashMap<Integer, Integer> queue; // K, C
-    private final HashMap<Integer, LinkedHashSet<Integer>> queueLRU;
+    private final HashMap<Integer, Integer> map; // Key, Value
+    private final HashMap<Integer, Integer> freqQueue; // Key, Сount
+    private final HashMap<Integer, LinkedList<Integer>> leastQueue ; // Сount, Key
     private final int capacity;
 
     public LFUCache(int capacity) {
         this.capacity = capacity;
         map = new HashMap<>(capacity);
-        queue = new HashMap<>(capacity);
-        queueLRU = new HashMap<>();
-        queueLRU.put(1, new LinkedHashSet<>());
+        freqQueue = new HashMap<>(capacity);
+        leastQueue = new HashMap<>();
+        leastQueue.put(1, new LinkedList<>());
     }
 
     public int get(int key) {
         if (!map.containsKey(key))
             return -1;
-        int count = queue.get(key);
-        queue.put(key, ++count);
+        int count = freqQueue.get(key);
+        leastQueue.get(count).remove((Integer) key);
+        freqQueue.put(key, ++count);
+        addFirst(key, count);
         return map.get(key);
     }
 
     public void put(int key, int value) {
+        if (capacity <=0 )
+            return;
+
         int count = 1;
         if (map.containsKey(key)) {
-            count = queue.get(key);
+            count = freqQueue.get(key);
+            leastQueue.get(count).remove((Integer) key);
             ++count;
         } else if (map.size() == capacity) {
-            var minKey = queue.entrySet()
-                    .stream()
-                    .min(Comparator.comparingInt(Map.Entry::getValue));
-            queue.remove(minKey.get().getKey());
-            map.remove(minKey.get().getKey());
+            for (var entity : leastQueue.entrySet()) {
+                if (entity.getValue().size() > 0) {
+                    int removeKey = entity.getValue().removeLast();
+                    freqQueue.remove(removeKey);
+                    map.remove(removeKey);
+                    break;
+                }
+            }
         }
-        queue.put(key, count);
+        freqQueue.put(key, count);
         map.put(key, value);
-        queueLRU.get(count).add(key);
+        addFirst(key, count);
+    }
+
+    private void addFirst(int key, int count) {
+        var obj = leastQueue.get(count);
+        if (obj != null) {
+            obj.addFirst(key);
+        } else {
+            LinkedList<Integer> objects = new LinkedList<>();
+            objects.addFirst(key);
+            leastQueue.put(count, objects);
+        }
     }
 
     public static void main(String[] args) {
-        LFUCache obj = new LFUCache(2);
+        LFUCache obj = new LFUCache(0);
+        obj.put(0, 0);
         obj.put(1, 1);
-        obj.put(2, 2);
-//        int param_1 = obj.get(1);
-        obj.put(1, 1);
-        int param_2 = obj.get(2);
-        int param_3 = obj.get(3);
+        int param_1 = obj.get(2);
+        int param_2 = obj.get(1);
+        int param_3 = obj.get(2);
+        obj.put(3, 3);
         obj.put(4, 4);
-        int param_4 = obj.get(1);
-        int param_5 = obj.get(3);
-        int param_6 = obj.get(4);
-
+        int param_4 = obj.get(3);
+        int param_5 = obj.get(2);
+        int param_6 = obj.get(1);
+        int param_7 = obj.get(4);
     }
 }
